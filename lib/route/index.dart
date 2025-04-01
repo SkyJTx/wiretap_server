9a939b7a4172a24a19e5866bae9e293f5432cdd2
Mr. Nattakarn Khumsupha <nattakarn.khumsupha.user1@outlook.com>
2025-04-01 13:58:16 +0700

    Add static file serving for public route with access token verification

--- lib/route/index.dart
+++ lib/route/index.dart
@@ -1,5 +1,6 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_static/shelf_static.dart';
 import 'package:wiretap_server/controller/token/verify_access.dart' as token;
 import 'package:wiretap_server/route/authentication.dart';
 import 'package:wiretap_server/route/session.dart';
@@ -17,4 +18,7 @@ final router =
         Pipeline()
             .addMiddleware(token.verifyAccessByToken(token.VerificationType.accessToken))
             .addHandler(sessionRouter.call),
+      )..get('/public', Pipeline()
+            .addMiddleware(token.verifyAccessByToken(token.VerificationType.accessToken))
+            .addHandler(createStaticHandler('public')),
       );
